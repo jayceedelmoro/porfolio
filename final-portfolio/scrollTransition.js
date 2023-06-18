4f4cf915926e7368bcb14bc69570eb975480b7d1
Carlo <jancarlodelmoro@gmail.com>
2023-06-18 18:00:31 +0800

    Fix: Converted navigation into JS

--- final-portfolio/scrollTransition.js
+++ final-portfolio/scrollTransition.js
@@ -1,20 +1,33 @@
 
 
 const scroll = (x, y) => {
-    window.scrollTo(x, y);
+    window.scrollTo( x, y );
 }
 
-scroll(0,0);
+scroll( 0,0 );
 
-const section = document.querySelectorAll('section');
-const scrollToTop = document.getElementById('scroll-to-top');
+const section = document.querySelectorAll( 'section' );
+const logo = document.querySelector( '.logo' );
+const home = document.querySelector( '.nav-menu-item.home' );
+const about = document.querySelector( '.nav-menu-item.about' );
+const skills = document.querySelector( '.nav-menu-item.skills' );
+const projects = document.querySelector( '.nav-menu-item.projects' );
+const contact = document.querySelector( '.nav-menu-item.contact' );
+const scrollToTop = document.getElementById( 'scroll-to-top' );
 
+const clientHeight = document.documentElement.clientHeight;
+
+logo.onclick = function() {scroll( 0,0 )};
+home.onclick = function() {scroll( 0,0 )};
+about.onclick = function() {scroll( 0, clientHeight )};
+skills.onclick = function() {scroll( 0, clientHeight * 2 )};
+projects.onclick = function() {scroll( 0, clientHeight * 3 )};
+contact.onclick = function() {scroll( 0, clientHeight * 4 )};
 scrollToTop.onclick = function() {scroll(0,0)};
 
 section[0].classList.add('sectionTranstion');
 
 document.addEventListener('scroll', () => {
-    const clientHeight = document.documentElement.clientHeight;
 
     section.forEach((s, index) => {
         let elemY = s.getBoundingClientRect().y;
